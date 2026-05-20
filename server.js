864e1c348d8b5c1c5c2d71a4ea7f2208fd17c352
MCroboOV12 <mcroboov12@gmail.com>
2026-05-20 13:36:35 +0200

    Use PORT env variable for Render

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
